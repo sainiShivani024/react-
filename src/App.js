cfb96e6774f8c7ad3ec35920050802e33b4d7a04
Shivani saini <sainishivani024@gmail.com>
2020-11-05 08:50:07 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Table from './src1/table'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Table />
+      </header>
+    </div>
+  );
+}
+
+export default App;
